878048a0a4a37a901405f3b033f229e15d78b819
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-07-31 14:08:20 -0300

    feat: create tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        primary: "#333",
       },
+      colors: {
+        secondary: '#111'
+      }
     },
   },
   plugins: [],
