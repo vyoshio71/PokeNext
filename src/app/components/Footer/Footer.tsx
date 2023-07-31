0ad68931e9597aa75ef0559290c2ca89192f1e55
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-07-31 14:08:38 -0300

    feat: create footer component

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="flex items-center justify-center bg-primary h-[137px] mt-8 border-t-2 border-solid border-secondary text-white">
+      <p>
+        <span className="font-bold">PokeNext</span> &copy; 2023
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
